492fe59f10d7ad4837a6fb28f213a525e7abc6ca
Tejas Kumar <tejas@tejas.qa>
2022-06-17 12:13:15 +0200

    Add alt fetch

---
+++ src/fetch-something-else.ts
@@ -0,0 +1,3 @@
+import fetch from "isomorphic-fetch";
+
+export const fetchSomethingElse = () => Promise.resolve('never gonna give you up\nnever gonna let you down\nnever gonna run you around\nand hurt you')
