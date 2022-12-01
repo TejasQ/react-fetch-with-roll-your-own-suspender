4e68c4fd456b8f249e9bf184fbd53ee53831a4f5
Tejas Kumar <tejas@tejas.qa>
2022-12-01 15:36:44 +1100

    Use suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, Suspense, useState } from "react";
 import Jokes from "./Jokes";
 
 const App: FC = () => {
@@ -7,21 +7,24 @@ const App: FC = () => {
   return (
     <main>
       <h1>The Jokes</h1>
-      <input
-        onChange={(e) => setSearch(e.target.value)}
-        type="text"
-        placeholder="Search..."
-      />
-      <p>
-        These are amazing developer jokes curated by{" "}
-        <a target="_blank" href="https://twitter.com/shrutikapoor08">
-          Shruti Kapoor
-        </a>{" "}
-        that make many of us laugh and I'm super thankful for her and I'm really
-        happy to share them all with you today at this amazing conference full
-        of awesome people. Thanks, GitNation for organizing this.
-      </p>
-      <Jokes search={search} />
+      <Suspense fallback="Stuff is loading...">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Search..."
+        />
+        <p>
+          These are amazing developer jokes curated by{" "}
+          <a target="_blank" href="https://twitter.com/shrutikapoor08">
+            Shruti Kapoor
+          </a>{" "}
+          that make many of us laugh and I'm super thankful for her and I'm
+          really happy to share them all with you today at this amazing
+          conference full of awesome people. Thanks, GitNation for organizing
+          this.
+        </p>
+        <Jokes search={search} />
+      </Suspense>
       <p>&copy; No one.</p>
     </main>
   );
--- src/Jokes.tsx
+++ src/Jokes.tsx
@@ -1,11 +1,67 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState, useTransition } from "react";
+
+const createSuspender = (somethingThatReturnsAPromise: () => Promise<any>) => {
+  let status = "pending";
+  let result: any;
+  let suspender = somethingThatReturnsAPromise().then(
+    (r) => {
+      status = "success";
+      result = r;
+    },
+    (e) => {
+      status = "error";
+      result = e;
+    }
+  );
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      } else if (status === "error") {
+        throw result;
+      } else if (status === "success") {
+        return result;
+      }
+    },
+  };
+};
+
+const createJokeResource = (search?: string) =>
+  createSuspender(() => {
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: process.env.XATA_API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: search
+        ? JSON.stringify({ filter: { joke: { $contains: search } } })
+        : '{"page":{"size":15}}',
+    };
+
+    const promise = fetch(
+      "https://xata-learning-fvmet3.eu-west-1.xata.sh/db/dev-jokes:main/tables/jokes/query",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => response.records);
+
+    return promise;
+  });
+
+const initialJokeResource = createJokeResource();
 
 const Jokes: FC<{ search: string }> = ({ search }) => {
-  const [jokes, setJokes] = React.useState<{ id: string; joke: string }[]>([]);
+  const [jokeResource, setJokeResource] = useState(initialJokeResource);
+  const [, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(() => setJokeResource(createJokeResource(search)));
+  }, [search]);
 
   return (
     <div className="jokes-container">
-      {jokes.map((j) => (
+      {jokeResource.read().map((j) => (
         <div className="joke" key={j.id}>
           {j.joke}
         </div>
